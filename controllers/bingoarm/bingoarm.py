8004ac3f1d229601aa4cb28f972d4ae2fc2abacf
Bradley <bradley.y.ma@gmail.com>
2021-04-14 16:21:25 -0600

    controller changes to pull out random shape to find

--- controllers/bingoarm/bingoarm.py
+++ controllers/bingoarm/bingoarm.py
@@ -4,6 +4,7 @@
 #  from controller import Robot, Motor, DistanceSensor
 from controller import Robot
 from controller import Camera
+import random
 
 
 # create the Robot instance.
@@ -21,11 +22,25 @@ timestep = int(robot.getBasicTimeStep())
 
 # camera.enable(1)
 joints = []
-for joint in ["shoulder_pan_joint", "shoulder_lift_join", "elbow_joint", "wrist_1_joint", "wrist_2_joint", "wrist_3_join"]:
+for joint in ["shoulder_pan_joint", "shoulder_lift_joint", "elbow_joint", "wrist_1_joint", "wrist_2_joint", "wrist_3_joint"]:
     joints.append(robot.getDevice(joint))
 counter = 0
 print(joints)
 
+counter = 0;
+
+random.seed(3)
+colors = ["blue", "brown", "green", "orange", "pink", "yellow"]
+shapes = ["circle", "diamond", "hexagon", "parallelogram", "trapezoid", "triangle"]
+possCombo = [(c, s) for c in colors for s in shapes]
+def chooseTarget():
+   
+    if len(possCombo) <= 0:
+        return None
+    choice = random.randrange(len(possCombo))
+    target = possCombo[choice]
+    possCombo.remove(target)
+    return target
 
 # Main loop:
 # - perform simulation steps until Webots is stopping the controller
@@ -35,9 +50,14 @@ while robot.step(timestep) != -1:
     #  val = ds.getValue()
 
     # Process sensor data here.
-
+    counter+=1
+    if(counter < 100):
+        print(counter, chooseTarget(), random.randrange(100))
     # Enter here functions to send actuator commands, like:
     #  motor.setPosition(10.0)
+    if not possCombo:
+        print(counter)
+        break
     pass
 
 # Enter here exit cleanup code.
